a3503af5fd52b4068293615e7aa158e2f6bbcb76
Victor Duarte <you@example.com>
2022-04-12 18:08:02 -0300

    Conexão estabelecida entre a seção de contatos e o MySQL

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var conn = require('./../inc/db');
 var express = require('express');
 var menus = require('./../inc/menus');
 var reservations = require('./../inc/reservations');
+var contacts = require('./../inc/contacts');
 var router = express.Router();
 
 /* GET home page. */
@@ -23,11 +24,28 @@ router.get('/', function(req, res, next) {
 
 router.get('/contacts', function(req, res, next){
 
-  res.render('contacts', {
-    title: 'Contato - Restaurante saboroso!',
-    background: 'images/img_bg_3.jpg',
-    h1: 'Diga um oi!',  
-  });
+  contacts.render(req, res);
+
+});
+
+router.post('/contacts', function(req, res, next){
+
+  if (!req.body.name) {
+    contacts.render(req, res, "Digite o nome.");
+  } else if (!req.body.email) {
+    contacts.render(req, res, "Digite o email.");
+  } else if (!req.body.message) {
+    contacts.render(req, res, "Insira sua mensagem.")
+  } else {
+    contacts.save(req.body).then(results =>{
+
+      contacts.render(req, res, null, "Mensagem enviada! Retornaremos assim que possível.")
+    
+    }).catch(err =>{
+      
+      contacts.render(req, res, err.message);
+    });
+  }
 
 });
 
