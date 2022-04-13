f0275e44e3724274373b3fcd7f65982b984ac795
Victor Duarte <victorctii217@gmail.com>
2022-04-13 14:58:55 -0300

    Criada autenticação pra login como admin

---
+++ inc/users.js
@@ -0,0 +1,44 @@
+var conn = require("./db");
+
+module.exports = {
+
+    render(req, res, error) {
+        res.render("admin/login", {
+            body: req.body,
+            error
+        });
+    },
+
+    // validando o login
+    login(email, password){
+        return new Promise ((resolve, reject)=>{
+
+            conn.query(`
+                SELECT * FROM tb_users WHERE email = ?
+            `, [
+                email
+            ], (err, results)=>{
+                if (err) {
+                    reject(error);
+                } else { 
+                    if (!results.length > 0) {
+                        reject("Usuário ou senha incorretos.");
+                    } else {
+
+                        let row = results[0];
+                        if (row.password !== password) {
+
+                            reject("Usuário ou senha incorretos.");
+
+                        } else {
+                            resolve(row);
+                        }
+                    }
+
+                    
+
+                }
+            });
+        });
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var users = require("./../inc/users");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
@@ -7,12 +8,30 @@ router.get("/", function(req, res, next) {
 
 });
 
+router.post("/login", function(req, res, next){
+    
+if(!req.body.email) {
+    users.render(req, res, "Insira o email.");
+} else if (!req.body.password) {
+    users.render(req, res, "Insira a senha.");
+} else {
+    
+    users.login(req.body.email, req.body.password).then(user =>{
+
+        req.session.user = user;
+
+        res.redirect("/admin");
+
+    }).catch(err=>{
+        users.render(req, res, err.message || err);
+    })
+}
+
+});
+
 router.get("/login", function(req, res, next) {
 
-    if (!req.session.views) req.session.views = 0;
-
-    console.log("SESSION", req.session.views++);
-    res.render("admin/login");
+    users.render(req, res, null);
 
 });
 
